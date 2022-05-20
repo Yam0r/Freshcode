eb1636dc4a001f40fb2b06522cbd562759096919
Yam0r <tortocool@gmail.com>
2022-05-20 11:03:01 +0300

    save

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,7 +1,8 @@
-let a, x;
-x = prompt("Value = ");
-if (x>=10) {
-    console.log("Больше");
-} else{
-    console.log("Меньше");
+function add(a,b){
+    let result = a + b;
+    return result;
 }
+
+let r;
+r = add(prompt('Число') + prompt("Число_2"));
+console.log(r)
