60b0909234dc0a388feb0f798696393c563cd9f1
Yam0r <tortocool@gmail.com>
2022-05-20 16:44:19 +0300

    Dz_JS

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,8 +1,70 @@
-function add(a,b){
-    let result = a + b;
-    return result;
+results()
+function results(){
+    let a;
+    a = prompt("Введите номер задания(1-5): ")
+    if(a == 1){
+        result_1()
+    }else if(a == 2){
+        result_2()
+    }else if(a == 3){
+        result_3()
+    }else if(a == 4){
+        result_4()
+    }else if(a == 5){
+        result_5()
+    } 
+    else{
+        alert("Такого задания нет: ");
+    }
+}
+function result_1() {
+    let a = 11,
+        b = true,
+        c = "java script",
+        d = "100";
+    console.log(a, b, c, d);
 }
 
-let r;
-r = add(prompt('Число') + prompt("Число_2"));
-console.log(r)
+function result_2() {
+    let greeting = "Hello, ",
+        userName;
+    userName = prompt("What is your name");
+    alert(greeting + userName);
+}
+
+function result_3() {
+    var min;
+    min = prompt("Веведите количество минут");
+    if (min >= 0 && min <= 15) {
+        alert("В первую четверть.");
+    }
+    if (min >= 16 && min <= 30) {
+        alert("Во вторую четверть.");
+    }
+    if (min >= 31 && min <= 45) {
+        alert("В третью четверть.");
+    }
+    if (min >= 46 && min <= 60) {
+        alert("В четвертую четверть.");
+    }
+}
+
+function result_4() {
+    for (let i = 10, b; i < 51; i++) {
+        if (i % 5 == 0) {
+            console.log(i);
+        }
+    }
+    console.log("End!");
+}
+
+function result_5() {
+    let result = 1,
+        a,
+        b;
+    for (let a = prompt("lim1: "), b = prompt("lim2: "); a <= b; a++) {
+        result *= a;
+    }
+    console.log(result);
+}
+
