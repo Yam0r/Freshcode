5291dd0e4d252c1ab6d8a3054e3508ce9976759d
Yam0r <tortocool@gmail.com>
2022-06-21 21:24:06 +0300

    commit

---
+++ JavaScript/js_DZ(25.05)/js/index.js
@@ -0,0 +1,16 @@
+class Worker{
+    constructor(options){
+        this.salary = options.salary,
+        this.name = options.name,
+        this.id = options.id
+    }
+}
+const worker = new Worker({
+    id:"117",
+    name: "Vasya",
+    salary:24000
+})
+console.log(worker);
+console.log("Зарплата за месяц: ", worker.salary);
+console.log("Зарплата за год: ", worker.salary*12);
+console.log("количество работников: ", worker.name);
---
+++ JavaScript/sandbox/js.js
@@ -0,0 +1,4 @@
+const btn = document.getElementById('btn');
+btn.onclick = function(){console.log('Hello')}
+
+btn.onclick = function(){alert('bye')}
